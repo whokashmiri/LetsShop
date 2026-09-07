b9e4dd79551eb4859fa618fa4d0902c7b7b0ddd9
Aaqib Bashir Mir <aaqibmir.ab@gmail.com>
2026-09-07 15:29:19 +0300

    register controller added

---
+++ src/main/java/com/ecom/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.ecom.controller;
+
+import com.ecom.dto.RegisterRequest;
+import com.ecom.models.User;
+import com.ecom.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService){
+        this.userService = userService;
+    }
+    @PostMapping("/register")
+    public ResponseEntity<User> create(@Valid  @RequestBody RegisterRequest  registerRequest){
+       User saved =  userService.register(registerRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+}
