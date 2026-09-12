d031f0a4b629b041fda404469289e983ae6d24f5
Aaqib Bashir Mir <aaqibmir.ab@gmail.com>
2026-09-12 21:38:19 +0300

    authentication

--- README.md
+++
@@ -1,1 +0,0 @@
-already committed 3 times
--- src/main/java/com/ecom/models/User.java
+++ src/main/java/com/ecom/models/User.java
@@ -41,6 +41,9 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+    public String getEmail(){
+        return email;
+    }
 
     public void setPhone(String phone) {
         this.phone = phone;
