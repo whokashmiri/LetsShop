dc251d232c5cfd2e2ca50ad33feb6e2b5fb6c438
Aaqib Bashir Mir <aaqibmir.ab@gmail.com.com>
2026-09-14 13:43:30 +0300

    Added role

--- src/main/java/com/ecom/config/SecurityConfig.java
+++ src/main/java/com/ecom/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 ).addFilterBefore(
                         jwtAuthenticationFilter ,
---
+++ src/main/java/com/ecom/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.ecom.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+    @GetMapping("/test")
+    public String admin(){
+        return "Admin Access Granted";
+    }
+}
